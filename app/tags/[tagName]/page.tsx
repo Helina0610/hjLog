129b38245aaa3b72bc4adc21056d30b6858556d0
Helina0610 <97938033+Helina0610@users.noreply.github.com>
2024-09-12 20:06:37 +0900

    feat : change module name

--- app/tags/[tagName]/page.tsx
+++ app/tags/[tagName]/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const page = () => {
+const TagName = () => {
   return (
-    <div>page</div>
+    <div>TagName</div>
   )
 }
 
-export default page
+export default TagName
